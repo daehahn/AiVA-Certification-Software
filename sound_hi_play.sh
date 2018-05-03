3616d54a5c8f8b9fd7107bd609a4b7f261930445
Roy Kang (Sungtae, Kang) <roykang75@gmail.com>
2018-05-03 12:16:55 +0900

    update sound script
    update sound script

--- sound_hi_play.sh
+++ sound_hi_play.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # 3.5mm jack
-#omxplayer -o local --loop sound/hi.mp3
+omxplayer -o local --loop sound/hi.mp3
 # USB speaker
-omxplayer -o alsa --loop sound/hi.mp3
+#omxplayer -o alsa --loop sound/hi.mp3
--- sound_pressure_play.sh
+++ sound_pressure_play.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # 3.5mm jack
-#omxplayer -o local --loop sound/1khz.mp3
+omxplayer -o local --loop sound/1khz.mp3
 # usb speaker
-omxplayer -o alsa --loop sound/1khz.mp3
+#omxplayer -o alsa --loop sound/1khz.mp3
