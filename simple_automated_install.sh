d226f38252955a0b026f05c88a3814ac73343df4
Roy Kang (Sungtae, Kang) <roykang75@gmail.com>
2018-07-18 15:47:25 +0900

    update
    update

--- simple_automated_install.sh
+++ simple_automated_install.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get upgrade -yq
 sudo apt-get install -y mpg123 python-serial
 
-echo "/home/linaro/workspace/AiVA-Cert-SW/menu.sh" | tee -a ~/.bashrc
+echo "/home/linaro/AiVA-Cert-SW/menu.sh" | tee -a ~/.bashrc
 source ~/.bashrc
 
 chmod +x *.sh
