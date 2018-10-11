91f3f9313c23d49b3fe77d3cc9332592cc5c1e31
Roy, Kang <stkang@inslab.co.kr>
2018-10-11 11:17:14 +0900

    remove files
    remove files

--- sound_hi_play.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 3.5mm jack
-omxplayer -o local --loop sound/hi.mp3
-# USB speaker
-#omxplayer -o alsa --loop sound/hi.mp3
--- sound_hi_play_db410.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while [[ 1 ]] ; do
-	mpg123 -q ./sound/hi.mp3
-done
-
